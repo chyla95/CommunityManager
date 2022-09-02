f58f26fd4b71a315fc9d808c7efce867394c6ad4
chyla95 <chyla95@gmail.com>
2022-09-02 02:43:18 +0200

    Fix Naming

--- src/middlewares/handle-exception.ts
+++ src/middlewares/handle-exception.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { CustomError } from "../errors/custom-error";
 
-export const exceptionHandler = (error: Error | CustomError, req: Request, res: Response, next: NextFunction) => {
+export const handleException = (error: Error | CustomError, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof CustomError) {
     return res.status(error.statusCode).send({ errors: error.serializeErrors() });
   }
--- src/middlewares/validate-route.ts
+++ src/middlewares/validate-route.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { NotFoundError } from "../errors/not-found-error";
 
-export const invalidRouteHandler = (req: Request, res: Response, next: NextFunction) => {
+export const validateRoute = (req: Request, res: Response, next: NextFunction) => {
   return next(new NotFoundError());
 };
