7899e275b5f5eefc3ffd2bfb37178bf7e4bebfe1
chyla95 <chyla95@gmail.com>
2022-09-02 20:46:29 +0200

    Add Request Validation Error Class

--- src/errors/request-validation-error.ts
+++ src/errors/request-validation-error.ts
@@ -1,18 +1,18 @@
-// import { ValidationError } from "express-validator";
-// import { CustomError } from "./custom-error";
+import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-// export class RequestValidationError extends CustomError {
-//   readonly statusCode = 400;
-//   readonly errors;
+export class RequestValidationError extends CustomError {
+  readonly statusCode = 400;
+  readonly errors;
 
-//   constructor(errors: ValidationError[]) {
-//     super("Invalid Parameters!");
-//     this.errors = errors;
-//   }
+  constructor(errors: ValidationError[], message: string = "Invalid Parameters!") {
+    super(message);
+    this.errors = errors;
+  }
 
-//   serializeErrors() {
-//     return this.errors.map((err) => {
-//       return { message: err.msg, field: err.param };
-//     });
-//   }
-// }
+  serializeErrors() {
+    return this.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
+  }
+}
