e68e963d749dfc165c65b3330936b4f4c43117de
chyla95 <chyla95@gmail.com>
2022-09-02 03:22:02 +0200

    Update Password Encryption - Efficiency

--- src/services/password.ts
+++ src/services/password.ts
@@ -1,24 +1,18 @@
 import { scrypt, randomBytes } from "crypto";
-import * as bcrypt from "bcrypt";
-
 import { promisify } from "util";
 
 const scryptAsync = promisify(scrypt);
 
 export class Password {
   static async encrypt(password: string) {
-    // const salt = randomBytes(8).toString("hex");
-    // const buffer = (await scryptAsync(password, salt, 64)) as Buffer;
-    // return `${buffer.toString("hex")}.${salt}`;
-
-    return await bcrypt.hash(password, 10);
+    const salt = randomBytes(8).toString("hex");
+    const buffer = (await scryptAsync(password, salt, 64)) as Buffer;
+    return `${buffer.toString("hex")}.${salt}`;
   }
 
   static async compare(storedPassword: string, suppliedPassword: string) {
-    // const [hashedPassword, salt] = storedPassword.split(".");
-    // const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
-    // return buffer.toString("hex") === hashedPassword;
-
-    return await bcrypt.compare(suppliedPassword, storedPassword);
+    const [hashedPassword, salt] = storedPassword.split(".");
+    const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+    return buffer.toString("hex") === hashedPassword;
   }
 }
