6e866eae9e0af8590061a270092a609ef0280e74
chyla95 <chyla95@gmail.com>
2022-10-11 23:59:25 +0200

    Fix Naming

--- src/middlewares/validate-request.ts
+++ src/middlewares/validate-request.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response, NextFunction } from "express";
 import { validationResult, ValidationChain } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
 
-const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
+export const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -12,6 +12,6 @@ const handleValidationErrors = (req: Request, res: Response, next: NextFunction)
   next();
 };
 
-export const validateRequestFields = (rules: ValidationChain[]) => {
+export const validateRequest = (rules: ValidationChain[]) => {
   return Router().use(...rules, handleValidationErrors);
 };
