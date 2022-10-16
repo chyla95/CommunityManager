6212ffad03020ad50310d25ddbee82ac9fcbe2c0
chyla95 <chyla95@gmail.com>
2022-10-17 01:35:23 +0200

    Update User Auth Middleware and Add Support For Suspended Accounts

--- src/middlewares/authenticate-user.ts
+++ src/middlewares/authenticate-user.ts
@@ -1,3 +1,25 @@
 import passport from "passport";
+import { Request, Response, NextFunction } from "express";
+import { AccountStatus, IUser } from "../models/user";
+import { NotAuthorizedError } from "../errors/not-authorized-error";
 
-export const authenticateUser = passport.authenticate("jwt", { session: false });
+// export const authenticateUser = passport.authenticate("jwt", { session: false });
+export const authenticateUser = (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate("jwt", { session: false }, (error, user: IUser, info) => {
+    if (error) return next(new Error(error));
+    if (!user) return next(new NotAuthorizedError("Not Authorized!"));
+    if (user.status == AccountStatus.Suspended) return next(new NotAuthorizedError("Account Suspended!"));
+
+    req.user = user;
+    return next();
+  })(req, res, next);
+};
+
+// Extends "req.employee" with "IEmployee" properties
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IUser;
+    }
+  }
+}
